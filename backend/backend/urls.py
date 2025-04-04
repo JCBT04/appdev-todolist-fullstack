ba731afb9ef2e32ef61b033d11567e3adbd60c77
jcbt <tevesjhunel01@gmail.com>
2025-04-05 02:51:06 +0800

    debug

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,9 +1,8 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import TemplateView  # For static views
+from django.urls import path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('todo.urls')),  # Keep this for the /api/
-    path('', TemplateView.as_view(template_name='index.html')),  # Serve a static page at the root
+    path('api/', include('todo.urls')),
+    path('', RedirectView.as_view(url='/api/', permanent=False)),  # Redirect root to /api/
 ]

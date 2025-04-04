6e34a14ab1072d75006b56bfcfaf1cdad55ad36f
jcbt <tevesjhunel01@gmail.com>
2025-04-05 03:05:51 +0800

    api

--- frontend/src/TodoList.jsx
+++ frontend/src/TodoList.jsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react";
 
-const API_URL = window.location.hostname === "localhost" 
-  ? "http://localhost:8000/api/todo/"  // Local development API URL
-  : "https://appdev-todolist-fullstack.onrender.com/api/todo/";  // Render production API URL
+// const API_URL = window.location.hostname === "localhost" 
+//   ? "http://localhost:8000/api/todo/"  // Local development API URL
+//   : "https://appdev-todolist-fullstack.onrender.com/api/todo/";  // Render production API URL
+
+const API_URL = "https://appdev-todolist-fullstack.onrender.com"
 
 export default function TodoList() {
     const [tasks, setTasks] = useState([]);
